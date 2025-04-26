2b41bd443bc8dff5e215cb5d3eb0a7e4c4335147
Edward Mangulenje <mangulenjeedward@gmail.com>
2025-04-25 21:24:32 -1200

    Fourth commit

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://the-backend-ytu0.onrender.com";
     const [token, setToken] = useState("");
     const [product_list, setProductList] = useState([])
 
