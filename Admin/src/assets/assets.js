dc82fc4a67a46e2775835284968e0a16d1c7343d
Eddie <mangulenjeedward@gmail.com>
2025-04-29 01:33:51 -1200

    Update assets.js

--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import logo from './logo.svg'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://urban-nest-backend-pvvj.onrender.com'
 
 export const assets ={
     logo1,
