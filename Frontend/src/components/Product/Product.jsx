3083eebed528e9902b2a095c0ef5919d120ca61e
Eddie <mangulenjeedward@gmail.com>
2025-05-02 01:28:54 -1200

    Update Product.jsx

--- Frontend/src/components/Product/Product.jsx
+++ Frontend/src/components/Product/Product.jsx
@@ -35,7 +35,7 @@ const Product = () => {
     return productData ? (
         <div className="product-page">
       <div className="product-image">
-        <img src={`https://urban-nest-backend-pvvj.onrender.com/images/${productData.image}`} alt={productData.name} />
+        <img src={`https://localhost:4000/images/${productData.image}`} alt={productData.name} />
           
       </div>
       <div className="product-details">
