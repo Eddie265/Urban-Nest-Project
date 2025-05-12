d602b502e60bb45886746ee4d6890d7a6b307aa1
Edward Mangulenje <mangulenjeedward@gmail.com>
2025-05-12 10:37:41 -1200

    tx commit

--- Frontend/src/components/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/components/PlaceOrder/PlaceOrder.jsx
@@ -115,7 +115,7 @@ const PlaceOrder = () => {
                         tx_ref: tx_ref,
                         amount: orderData.amount,
                         currency: "MWK",
-                        callback_url: `${url}/api/order/verify/paychangu`,
+                        callback_url: `${url}/api/order/verify/paychangu?tx_ref=${tx_ref}`,
                         return_url: `${window.location.origin}/verify?success=true&tx_ref=${tx_ref}&orderId=${orderId}&paymentMethod=paychangu`,
                         customer: {
                             email: data.email,
